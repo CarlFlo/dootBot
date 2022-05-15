9876898dd3df81edd31b9a1ab9588b2d5dd973dc
CarlFlo <Carl.flodin@hotmail.com>
2022-05-15 20:43:09 +0200

    Reworked daily code

--- bot/commands/daily/daily.go
+++ bot/commands/daily/daily.go
@@ -2,12 +2,10 @@ package daily
 
 import (
 	"fmt"
-	"math/rand"
 
 	"github.com/CarlFlo/DiscordMoneyBot/bot/structs"
 	"github.com/CarlFlo/DiscordMoneyBot/config"
 	"github.com/CarlFlo/DiscordMoneyBot/database"
-	"github.com/CarlFlo/DiscordMoneyBot/utils"
 	"github.com/CarlFlo/malm"
 	"github.com/bwmarrin/discordgo"
 )
@@ -20,11 +18,46 @@ func Daily(s *discordgo.Session, m *discordgo.MessageCreate, input *structs.CmdI
 	var daily database.Daily
 	daily.GetDailyInfo(&user)
 
-	// Reset streak if user hasn't done their daily in a specified amount of time (set in config)
-	daily.CheckStreak()
+	ok, earnedMoney, streakReward, streakPercentage, titleText, footerText := daily.DoDaily(&user)
 
-	complexMessage := &discordgo.MessageSend{}
-	dailyMessageBuilder(complexMessage, m, &user, &daily)
+	var description string
+	var color int
+
+	if ok {
+		color = config.CONFIG.Colors.Success
+		description = fmt.Sprintf("%sYou earned ``%s`` %s! Your new balance is ``%s`` %s!\nYou will be able to get your daily again %s\nCurrent streak: ``%d``",
+			config.CONFIG.Emojis.Economy,
+			earnedMoney,
+			config.CONFIG.Economy.Name,
+			user.PrettyPrintMoney(),
+			config.CONFIG.Economy.Name,
+			daily.CanDoDailyAt(),
+			daily.ConsecutiveStreaks)
+	} else {
+		color = config.CONFIG.Colors.Failure
+		description = fmt.Sprintf("You can get your next daily again %s", daily.CanDoDailyAt())
+	}
+
+	complexMessage := &discordgo.MessageSend{
+		Embed: &discordgo.MessageEmbed{
+			Type:        discordgo.EmbedTypeRich,
+			Color:       color,
+			Title:       titleText,
+			Description: description,
+			Fields: []*discordgo.MessageEmbedField{
+				{
+					Name:  fmt.Sprintf("Extra Reward Progress (%s)", streakPercentage),
+					Value: streakReward,
+				},
+			},
+			Thumbnail: &discordgo.MessageEmbedThumbnail{
+				URL: fmt.Sprintf("%s#%s", m.Author.AvatarURL("256"), m.Author.ID),
+			},
+			Footer: &discordgo.MessageEmbedFooter{
+				Text: footerText,
+			},
+		},
+	}
 
 	// Sends the message
 	if _, err := s.ChannelMessageSendComplex(m.ChannelID, complexMessage); err != nil {
@@ -37,114 +70,4 @@ func Daily(s *discordgo.Session, m *discordgo.MessageCreate, input *structs.CmdI
 
 	user.Save()
 	daily.Save()
-}
-
-func dailyMessageBuilder(msg *discordgo.MessageSend, m *discordgo.MessageCreate, user *database.User, daily *database.Daily) {
-
-	if daily.CanDoDaily() {
-
-		daily.UpdateStreakAndTime()
-
-		// Calculates the income
-		moneyEarned := generateDailyIncome(daily)
-		user.AddMoney(uint64(moneyEarned))
-
-		moneyEarnedString := utils.HumanReadableNumber(moneyEarned)
-
-		extraRewardValue, percentage := generateDailyStreakMessage(daily.Streak, true)
-
-		description := fmt.Sprintf("%sYou earned ``%s`` %s! Your new balance is ``%s`` %s!\nYou will be able to get your daily again %s\nCurrent streak: ``%d``",
-			config.CONFIG.Emojis.Economy,
-			moneyEarnedString,
-			config.CONFIG.Economy.Name,
-			user.PrettyPrintMoney(),
-			config.CONFIG.Economy.Name,
-			daily.CanDoDailyAt(),
-			daily.ConsecutiveStreaks)
-
-		msg.Embeds = []*discordgo.MessageEmbed{
-			{
-				Type:        discordgo.EmbedTypeRich,
-				Color:       config.CONFIG.Colors.Success,
-				Title:       "Daily Bonus",
-				Description: description,
-				Fields: []*discordgo.MessageEmbedField{
-					{
-						Name:  fmt.Sprintf("Extra Reward Progress (%s)", percentage),
-						Value: extraRewardValue,
-					},
-				},
-				Footer: &discordgo.MessageEmbedFooter{
-					Text: fmt.Sprintf("Completing your streak will earn you an extra %d %s!\nThe streak resets after %d hours of inactivity.",
-						config.CONFIG.Daily.StreakBonus,
-						config.CONFIG.Economy.Name,
-						config.CONFIG.Daily.StreakResetHours),
-				},
-			},
-		}
-	} else {
-
-		description := fmt.Sprintf("You can get your next daily again %s", daily.CanDoDailyAt())
-		extraRewardValue, percentage := generateDailyStreakMessage(daily.Streak, false)
-
-		msg.Embeds = []*discordgo.MessageEmbed{
-			{
-				Type:        discordgo.EmbedTypeRich,
-				Color:       config.CONFIG.Colors.Failure,
-				Title:       fmt.Sprintf("%s Slow down!", config.CONFIG.Emojis.Failure),
-				Description: description,
-				Fields: []*discordgo.MessageEmbedField{
-					{
-						Name:  fmt.Sprintf("Extra Reward Progress (%s)", percentage),
-						Value: extraRewardValue,
-					},
-				},
-				Footer: &discordgo.MessageEmbedFooter{
-					Text: fmt.Sprintf("You can get your daily once every %d hours!", int(config.CONFIG.Daily.Cooldown)),
-				},
-			},
-		}
-	}
-
-	msg.Embeds[0].Thumbnail = &discordgo.MessageEmbedThumbnail{
-		URL: fmt.Sprintf("%s#%s", m.Author.AvatarURL("256"), m.Author.ID),
-	}
-}
-
-func generateDailyStreakMessage(streak uint16, addStreakMessage bool) (string, string) {
-
-	percentage := float64(streak) / float64(len(config.CONFIG.Daily.StreakOutput))
-	upTo := int(float64(len(config.CONFIG.Daily.StreakOutput)) * percentage)
-
-	// Append to a string values in config.CONFIG.Daily.StreakOutput up to the index of upTo
-	var visualStreakProgress string
-
-	for i := 0; i < upTo; i++ {
-		visualStreakProgress += fmt.Sprintf("%s ", config.CONFIG.Daily.StreakOutput[i])
-	}
-	for i := upTo; i < len(config.CONFIG.Daily.StreakOutput); i++ {
-		visualStreakProgress += "- "
-	}
-
-	percentageText := fmt.Sprintf("%d%%", int(percentage*100))
-
-	var streakMessage string
-	if addStreakMessage && streak == uint16(len(config.CONFIG.Daily.StreakOutput)) {
-		streakMessage = fmt.Sprintf("An additional ``%s`` %s were added to your daily earnings!", utils.HumanReadableNumber(config.CONFIG.Daily.StreakBonus), config.CONFIG.Economy.Name)
-	}
-
-	return fmt.Sprintf("%s %s", visualStreakProgress, streakMessage), percentageText
-}
-
-func generateDailyIncome(daily *database.Daily) int {
-
-	// Generate a random int between config.CONFIG.Daily.MinMoney and config.CONFIG.Daily.MaxMoney
-	moneyEarned := rand.Intn(config.CONFIG.Daily.MaxMoney-config.CONFIG.Daily.MinMoney) + config.CONFIG.Daily.MinMoney
-
-	// Adds the streak bonus to the amount
-	if daily.Streak == uint16(len(config.CONFIG.Daily.StreakOutput)) {
-		moneyEarned += config.CONFIG.Daily.StreakBonus
-	}
-
-	return moneyEarned
 }
--- database/schemaDaily.go
+++ database/schemaDaily.go
@@ -2,9 +2,11 @@ package database
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 
 	"github.com/CarlFlo/DiscordMoneyBot/config"
+	"github.com/CarlFlo/DiscordMoneyBot/utils"
 	"gorm.io/gorm"
 )
 
@@ -36,6 +38,85 @@ func (d *Daily) GetDailyInfo(u *User) {
 	}
 }
 
+// Does the daily
+// Returns
+// If the daily could be executed
+// The money earned as a pretty string
+// Streak reward value
+// Percentage of streak completed
+// Title for the message
+// Footer for the message
+func (d *Daily) DoDaily(user *User) (bool, string, string, string, string, string) {
+
+	// Resets streaks down to 0 if the user failed their streak.
+	d.checkStreak()
+
+	// Can't do their daily
+	if !d.CanDoDaily() {
+		streakReward, streakPercentage := d.generateDailyStreakMessage()
+		return false,
+			"",
+			streakReward,
+			streakPercentage,
+			fmt.Sprintf("%s Slow down!", config.CONFIG.Emojis.Failure),
+			fmt.Sprintf("You can get your daily once every %d hours!", int(config.CONFIG.Daily.Cooldown))
+	}
+
+	d.updateStreakAndTime()
+
+	moneyEarned := d.generateDailyIncome()
+	user.AddMoney(uint64(moneyEarned))
+
+	moneyEarnedString := utils.HumanReadableNumber(moneyEarned)
+	streakReward, streakPercentage := d.generateDailyStreakMessage()
+
+	title := "Daily Bonus"
+	footer := fmt.Sprintf("Completing your streak will earn you an extra %d %s!\nThe streak resets after %d hours of inactivity.",
+		config.CONFIG.Daily.StreakBonus,
+		config.CONFIG.Economy.Name,
+		config.CONFIG.Daily.StreakResetHours)
+
+	return true, moneyEarnedString, streakReward, streakPercentage, title, footer
+}
+
+func (d *Daily) generateDailyIncome() int {
+
+	// Generate a random int between config.CONFIG.Daily.MinMoney and config.CONFIG.Daily.MaxMoney
+	moneyEarned := rand.Intn(config.CONFIG.Daily.MaxMoney-config.CONFIG.Daily.MinMoney) + config.CONFIG.Daily.MinMoney
+
+	// Adds the streak bonus to the amount
+	if d.Streak == uint16(len(config.CONFIG.Daily.StreakOutput)) {
+		moneyEarned += config.CONFIG.Daily.StreakBonus
+	}
+
+	return moneyEarned
+}
+
+func (d *Daily) generateDailyStreakMessage() (string, string) {
+
+	percentage := float64(d.Streak) / float64(len(config.CONFIG.Daily.StreakOutput))
+	upTo := int(float64(len(config.CONFIG.Daily.StreakOutput)) * percentage)
+
+	// Append to a string values in config.CONFIG.Daily.StreakOutput up to the index of upTo
+	var visualStreakProgress string
+
+	for i := 0; i < upTo; i++ {
+		visualStreakProgress += fmt.Sprintf("%s ", config.CONFIG.Daily.StreakOutput[i])
+	}
+	for i := upTo; i < len(config.CONFIG.Daily.StreakOutput); i++ {
+		visualStreakProgress += "- "
+	}
+
+	percentageText := fmt.Sprintf("%d%%", int(percentage*100))
+
+	var streakMessage string
+	if d.CanDoDaily() && d.Streak == uint16(len(config.CONFIG.Daily.StreakOutput)) {
+		streakMessage = fmt.Sprintf("An additional ``%s`` %s were added to your daily earnings!", utils.HumanReadableNumber(config.CONFIG.Daily.StreakBonus), config.CONFIG.Economy.Name)
+	}
+
+	return fmt.Sprintf("%s %s", visualStreakProgress, streakMessage), percentageText
+}
+
 // CanDoDaily - Checks if the user can do their daily again
 // Returns true if the user can do their daily and false if they cant
 func (d *Daily) CanDoDaily() bool {
@@ -51,19 +132,19 @@ func (d *Daily) CanDoDailyAt() string {
 	return fmt.Sprintf("<t:%d:R>", nextTime)
 }
 
-// CheckStreak - Checks the streak for the work object
+// checkStreak - Checks the streak for the work object
 // Resets it down to 0 if the user failed their streak. i.e. Waited too long since the last work
-func (d *Daily) CheckStreak() {
+func (d *Daily) checkStreak() {
 	if time.Since(d.LastDailyAt).Hours() > float64(config.CONFIG.Daily.StreakResetHours) {
 		d.ConsecutiveStreaks = 0
 		d.Streak = 0
 	}
 }
 
-// UpdateStreakAndTime - Updates the daily streak for the user i.e. adding one to the counters
+// updateStreakAndTime - Updates the daily streak for the user i.e. adding one to the counters
 // and ensuring the streak is not over the max streak
 // and updating the time of the last daily
-func (d *Daily) UpdateStreakAndTime() {
+func (d *Daily) updateStreakAndTime() {
 	// Updates the variables
 	d.LastDailyAt = time.Now()
 
